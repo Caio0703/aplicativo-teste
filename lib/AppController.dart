4aac46ff7cca37654ed02a13c58767e2991d5a65
unknown <catce2020111iinf0364@aluno.ifpi.edu.br>
2022-06-09 19:18:41 -0300

    ramo1

--- lib/AppController.dart
+++ lib/AppController.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppController extends ChangeNotifier{
+static AppController instance=AppController();
 bool isDartTheme=false;
 ChangeTheme(){isDartTheme=!isDartTheme;
  notifyListeners();}
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:aplicativo/AppController.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,11 +17,23 @@ class HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('WhatsApp'),backgroundColor: Colors.green,
-        actions:[],
+        actions: [CustomSwitch()],
+        
+        
         
       ),
       floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.add),),
     );
     throw UnimplementedError();
+  }
+}
+class CustomSwitch extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(value:AppController.instance.isDartTheme,onChanged:(value){AppController.instance.ChangeTheme();}
+      
+    );
   }
 }
